bc0d0822267c5aa48df05afae2dc91ae4ebf3fa2
Petter Hodt <petterhodt@gmail.com>
2026-02-09 12:03:45 +0100

    last opp

---
+++ skeleton_project/source/driver/elevio.c
@@ -0,0 +1,132 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include "elevio.h"
+#include "con_load.h"
+
+static int sockfd;
+static pthread_mutex_t sockmtx;
+
+//initialisering
+void elevio_init(void){
+    char ip[16] = "localhost";
+    char port[8] = "15657";
+    con_load("source/driver/elevio.con",
+        con_val("com_ip",   ip,   "%s")
+        con_val("com_port", port, "%s")
+    )
+    
+    pthread_mutex_init(&sockmtx, NULL);
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sockfd != -1 && "Unable to set up socket");
+    
+    struct addrinfo hints = {
+        .ai_family      = AF_INET, 
+        .ai_socktype    = SOCK_STREAM, 
+        .ai_protocol    = IPPROTO_TCP,
+    };
+    struct addrinfo* res;
+    getaddrinfo(ip, port, &hints, &res);
+    
+    int fail = connect(sockfd, res->ai_addr, res->ai_addrlen);
+    assert(fail == 0 && "Unable to connect to elevator server");
+    
+    freeaddrinfo(res);
+    
+    send(sockfd, (char[4]){0}, 4, 0);
+}
+
+
+
+
+void elevio_motorDirection(MotorDirection dirn){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){1, dirn}, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+}
+
+
+void elevio_buttonLamp(int floor, ButtonType button, int value){
+    //assert - hvis uttrykk false - gir feilmelding
+    assert(floor >= 0);
+    assert(floor < N_FLOORS);
+    assert(button >= 0);
+    assert(button < N_BUTTONS);
+
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){2, button, floor, value}, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+}
+
+
+void elevio_floorIndicator(int floor){
+    assert(floor >= 0);
+    assert(floor < N_FLOORS);
+
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){3, floor}, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+}
+
+
+void elevio_doorOpenLamp(int value){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){4, value}, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+}
+
+//skrur AV/PÅ stopplampen for heispanel
+// 1 = PÅ, 0 = AV
+void elevio_stopLamp(int value){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){5, value}, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+}
+
+
+
+//hvilken knapp trykket 
+int elevio_callButton(int floor, ButtonType button){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){6, button, floor}, 4, 0);
+    char buf[4]; //array ned 4 bytes (4 char) lagrer tom buffer
+    recv(sockfd, buf, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+    return buf[1]; //2 element = DATA
+}
+
+//hvilken etasje vi er på / eller om vi ikke er på etasje
+int elevio_floorSensor(void){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){7}, 4, 0);
+    char buf[4];
+    recv(sockfd, buf, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+    return buf[1] ? buf[2] : -1; //buf[1] - er vi på en etasje, buf[2]- hvilken etasje er vi på ?, hvis ikke ret -1
+}
+
+//sjekker om stop knapp trykket
+int elevio_stopButton(void){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){8}, 4, 0);
+    char buf[4];
+    recv(sockfd, buf, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+    return buf[1];
+}
+
+//sjekker om obstruction bryter på/av
+int elevio_obstruction(void){
+    pthread_mutex_lock(&sockmtx);
+    send(sockfd, (char[4]){9}, 4, 0);
+    char buf[4];
+    recv(sockfd, buf, 4, 0);
+    pthread_mutex_unlock(&sockmtx);
+    return buf[1];
+}
---
+++ skeleton_project/source/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include "driver/elevio.h"
+
+
+
+int main(){
+    elevio_init();
+    
+    printf("=== Example Program ===\n");
+    printf("Press the stop button on the elevator panel to exit\n");
+
+    elevio_motorDirection(DIRN_UP);
+
+    while(1){
+        int floor = elevio_floorSensor();
+
+        if(floor == 0){
+            elevio_motorDirection(DIRN_UP);
+        }
+
+        if(floor == N_FLOORS-1){
+            elevio_motorDirection(DIRN_DOWN);
+        }
+
+
+        for(int f = 0; f < N_FLOORS; f++){
+            for(int b = 0; b < N_BUTTONS; b++){ //b =type knapp, hall up, eller hall down, eller cab
+                int btnPressed = elevio_callButton(f, b); // finner hvilken knapp som er trykket på 
+                elevio_buttonLamp(f, b, btnPressed); //den knappen som er trykket skal lyse
+            }
+        }
+
+        if(elevio_obstruction()){
+            elevio_stopLamp(1);
+        } else {
+            elevio_stopLamp(0);
+        }
+        
+        if(elevio_stopButton()){
+            elevio_motorDirection(DIRN_STOP);
+            break;
+        }
+        
+        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL); //sover i 20ms
+    }
+
+    return 0;
+}
